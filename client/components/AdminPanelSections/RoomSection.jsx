8db25e6459f8c9aa846656ccdaf9b50e3c6a80f1
toliksem1 <154976788+toliksem1@users.noreply.github.com>
2026-09-25 21:13:32 +0200

    Update RoomSection.jsx

--- client/components/AdminPanelSections/RoomSection.jsx
+++ client/components/AdminPanelSections/RoomSection.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Map, List, fromJS} from 'immutable';
 
-import {roomSetSeedRequest, roomStartVotingRequest} from '../../../shared/actions/actions';
+import {roomSetSeedRequest, roomStartVotingRequest, roomEditSettingsRequest} from '../../../shared/actions/actions.js';
 
 const defaultGameSeed = `deck: 12 carnivorous, 6 sharp
 phase: feeding
@@ -27,21 +27,38 @@ export class RoomSection extends Component {
     this.setState({gameSeed})
   }
 
-  render() {
-    return <div>
-      <h6 className="pointer" onClick={() => 0}>Add bot</h6>
-      {this.props.gameCanStart
-        ? <h6 className="pointer" onClick={this.props.$start(this.props.roomId, this.state.gameSeed)}>Start Game ►</h6>
-        : null}
-      <div>
-        <textarea
-          rows={8} cols={40}
-          value={this.state.gameSeed}
-          style={{overflow: 'hidden'}}
-          onChange={(e) => this.setGameSeed(e.target.value)}/>
-      </div>
+render() {
+  return <div>
+    <h6 className="pointer" onClick={() => 0}>Add bot</h6>
+
+    <div style={{marginBottom: '10px'}}>
+      <input
+        type="number"
+        placeholder="Количество еды"
+        value={this.state.fixedFoodInput || ''}
+        onChange={(e) => this.setState({fixedFoodInput: e.target.value})}
+        style={{padding: '5px', width: '100px'}}
+      />
+      <h6
+        className="pointer"
+        onClick={() => this.props.$setFixedFood(this.props.room, this.state.fixedFoodInput)}
+        style={{display: 'inline', marginLeft: '10px'}}
+      >
+        Установить еду
+      </h6>
     </div>
-  }
+
+    {this.props.gameCanStart
+      ? <h6 className="pointer" onClick={this.props.$start(this.props.roomId, this.state.gameSeed)}>Start Game ►</h6>
+      : null}
+    <div>
+      <textarea
+        rows={8} cols={40}
+        value={this.state.gameSeed}
+        style={{overflow: 'hidden'}}
+        onChange={(e) => this.setGameSeed(e.target.value)}/>
+    </div>
+  </div>
 }
 
 export const RoomSectionView = connect(
@@ -51,15 +68,40 @@ export const RoomSectionView = connect(
     const room = state.getIn(['rooms', roomId]);
     const gameCanStart = room ? room.checkCanStart(userId) : false;
     return {
-      roomId
-      , userId
-      , gameCanStart
-    }
-  }
-  , (dispatch) => ({
+      roomId,
+      userId,
+      room,
+      gameCanStart
+    };
+  },
+  (dispatch) => ({
     $start: (roomId, seed) => () => {
       dispatch(roomSetSeedRequest(seed));
       dispatch(roomStartVotingRequest());
+    },
+    $setFixedFood: (room, fixedFood) => () => {
+      if (!fixedFood || isNaN(fixedFood)) {
+        alert('Введи число');
+        return;
+      }
+
+      dispatch(roomEditSettingsRequest({
+        name: room.name,
+        maxPlayers: room.settings.maxPlayers,
+        timeTurn: room.settings.timeTurn / 1000,
+        timeTraitResponse: room.settings.timeTraitResponse / 1000,
+        randomPlayers: room.settings.randomPlayers,
+        halfDeck: room.settings.halfDeck,
+        maxCards: room.settings.maxCards,
+        addon_base2: room.settings.addon_base2,
+        addon_timeToFly: room.settings.addon_timeToFly,
+        addon_continents: room.settings.addon_continents,
+        addon_bonus: room.settings.addon_bonus,
+        addon_plantarium: room.settings.addon_plantarium,
+        addon_customff: room.settings.addon_customff,
+        addon_lifecycle: room.settings.addon_lifecycle,
+        fixedFood: parseInt(fixedFood)
+      }));
     }
   })
 )(RoomSection);
